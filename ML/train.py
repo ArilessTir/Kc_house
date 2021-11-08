99eddcefdd43f258e5ade720cdb05ddd23b0439b
ArilessTir <ariless.tir@gmail.com>
2021-11-08 14:58:28 +0100

    update requirements

--- ML/train.py
+++ ML/train.py
@@ -5,23 +5,28 @@ from functions.get_data import get_dataset, get_Xy
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 
-data = get_dataset()
 
-X,y = get_Xy(data, 'price')
+def run_training():
+    data = get_dataset()
+
+    X,y = get_Xy(data, 'price')
 
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
-
-
-if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 
     print('training....')
 
+    ###### Fiting
+
     model = pipeline.model.fit(X_train,y_train)
-    y_pred = pipeline.model.predict(X_test)
+    y_pred = model.predict(X_test)
 
     ###### Metrics
 
-    print(np.sqrt(mean_squared_error(y_test, y_pred)))
-    print(r2_score(y_test, y_pred))
+    print(f'RMSE:{np.sqrt(mean_squared_error(y_test, y_pred))}')
+    print(f'R²:{r2_score(y_test, y_pred)*100}')
     
+
+if __name__ == '__main__':
+    run_training()
+
